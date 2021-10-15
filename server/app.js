de98673075bf3748d83a567c8792d7bbf746d1c7
Emil Christiansen <emilsen68@gmail.com>
2021-10-16 00:49:12 +0200

    Everything is done
    Didn't really make this is pieces. Still haven't added the part that removes expired pending confirmations, but that is coming

---
+++ server/app.js
@@ -0,0 +1,148 @@
+// All external modules are loaded in:
+const express = require("express")
+const app = express()
+const path = require("path")
+const fs = require("fs")
+const cors = require("cors")
+const nodemailer = require("nodemailer")
+const emailCheck = require("email-check")
+
+const serverAddress = "http://172.16.4.72:3000"
+
+let connections = 0
+
+function loadJSON(filename) {
+    const rawdata = fs.readFileSync(path.join(__dirname, filename))
+    const data = JSON.parse(rawdata)
+    return data
+}
+
+function saveJSON(json, filename) {
+    const stringified = JSON.stringify(json, null, 4)
+    fs.writeFile(path.join(__dirname, filename), stringified, (err) => {
+        if (err) throw err
+        console.log("Data written to file")
+    })
+}
+
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+mailPass = loadJSON("/mailPass.json")
+console.log(mailPass)
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "emilsen68@gmail.com",
+        pass: mailPass
+    }
+})
+
+
+
+// Reading input from terminal start
+const port = parseInt(process.argv[2])
+console.log(`${port} registered as server port`)
+// Reading input from terminal end
+
+app.use(cors()) // Making sure the browser can request more data after it is loaded on the client computer.
+
+app.use("/static", express.static("public"))
+
+app.get("/", (req, res) => {
+    connections++
+    console.log(`Connections made since server started: ${connections}`)
+    res.sendFile(path.join(__dirname, "/html/index.html"))
+})
+
+app.get("/mailtest", (req, res) => {
+
+    emailCheck(req.query.mail)
+        .then((response) => {
+            console.log(response)
+            res.send(response)
+        })
+        .catch((err) => {
+            if (err.message === "refuse") {
+                console.error("MX server is refusing requests from your IP address")
+                res.send(false)
+            } else {
+                console.error(err)
+                res.send(false)
+            }
+        })
+})
+
+app.get("/reserveseat", (req, res) => {
+    let reserved = loadJSON("/public/json/occupiedSeats.json")
+    let pendingSeats = loadJSON("/public/json/pendingSeats.json")
+
+    if (req.query.seat in reserved || req.query.seat in pendingSeats) {
+        console.log("Seat already reserved")
+        res.send({"response": "Already reserved"})
+    }
+    else {
+        res.send({"response": "Success"})
+        console.log("Success")
+        const randCode = randInt(1111111111, 9999999999)
+        
+        pendingSeats[req.query.seat] = {
+            "name": req.query.name,
+            "mail": req.query.mail,
+            "code": randCode
+        }
+        saveJSON(pendingSeats, "/public/json/pendingSeats.json")
+
+        const mailOptions = {
+            from: "emilsen68@gmail.com",
+            to: req.query.mail,
+            subject: "Elevkveld reservasjon bekreftelse",
+            text: `Klikk på denne linken for å bekrefte din reservasjon: ${serverAddress}/confirm?code=${randCode}`
+        }
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log(error)
+                res.send(error)
+            }
+            else {
+                console.log(`Email sent: ${info.response}`)
+                res.send(`Email sent: ${info.response}`)
+            }
+        })
+    }
+})
+
+app.get("/confirm", (req, res) => {
+    const pendingSeats = loadJSON("/public/json/pendingSeats.json")
+
+    let reservationFound = false
+    let confirmedReservation = ""
+    for (const [k, v] of Object.entries(pendingSeats)) {
+        if (req.query.code == (v.code)) {
+            res.send("Reservasjonen har blitt bekreftet")
+            reservationFound = true
+            confirmedReservation = k
+            break
+        }
+    }
+    if (reservationFound) {
+        const occupiedSeats = loadJSON("/public/json/occupiedSeats.json")
+
+        occupiedSeats[confirmedReservation] = {
+            "name": pendingSeats[confirmedReservation].name,
+            "mail": pendingSeats[confirmedReservation].mail
+        }
+        delete pendingSeats[confirmedReservation]
+
+        saveJSON(occupiedSeats, "/public/json/occupiedSeats.json")
+        saveJSON(pendingSeats, "/public/json/pendingSeats.json")
+    }
+    else {
+        res.send("Oida! Den reservasjonen finnes ikke!\nHvis det er mer enn 5 minutter siden du reserverte setet, har tiden gått ut!")
+    }
+})
+
+app.listen(port, () => console.log(`Listening on ${port}`))
---
+++ server/public/javascript/seatSelectionHandler.js
@@ -0,0 +1,116 @@
+function seatSelectionhandler() {
+
+    submitButton = document.getElementById("submit")
+    Name = document.getElementById("name")
+    mail = document.getElementById("mail")
+
+    let seatSelected = {
+        state: false,
+        seat: ""
+    }
+    function populateSeats() {
+
+        table = document.getElementById("table")
+        table.innerHTML = ""
+        const occupiedList = getJSON(`${url}/static/json/occupiedSeats.json`)
+        const pendingList = getJSON(`${url}/static/json/pendingSeats.json`)
+
+        for (let i = 0; i < 7; i++) {
+            tr = document.createElement("tr")
+
+            for (let y = 0; y < 12; y++) {
+                td = document.createElement("td")
+                button = document.createElement("input")
+                button.type = "button"
+                button.id = `${i}:${y}`
+
+                if (button.id in occupiedList) {
+                    console.log(`${button.id} is occupied by ${occupiedList[button.id].name}`)
+                    button.style = "background-color: #ff4a4a"
+                }
+                else if (button.id in pendingList) {
+                    console.log(`${button.id} is awaiting confirmation from ${pendingList[button.id].name}`)
+                    button.style = "background-color: #ff4a4a"
+                }
+
+                button.onclick = (event) => {
+                    console.log(event.target.id)
+                    if (button.id in occupiedList) {
+                        alert("Sete er ikke ledig")
+                    }
+                    else {
+
+                        const occupiedList = getJSON(`${url}/static/json/occupiedSeats.json`)
+                        const pendingList = getJSON(`${url}/static/json/pendingSeats.json`)
+                        if (event.target.id in occupiedList || event.target.id in pendingList) {
+                            document.getElementById(event.target.id).style = "background-color: #ff4a4a"
+                        }else {
+                            if (seatSelected.state) {
+                                document.getElementById(seatSelected.seat).style = ""
+                            }
+    
+                            document.getElementById(event.target.id).style = "background-color: #82ff86"
+                            seatSelected.state = true
+                            seatSelected.seat = event.target.id
+                        }
+                    }
+                    populateSeats()
+                            if (seatSelected.state) {
+                                document.getElementById(seatSelected.seat).style = "background-color: #82ff86"
+                            }
+                }
+
+                td.appendChild(button)
+                tr.appendChild(td)
+            }
+            table.appendChild(tr)
+        }
+
+    }populateSeats()
+
+    submitButton.addEventListener("click", (event) => {
+        console.log(`${Name.value}, ${mail.value}`)
+        const mailExists = getJSON(`${url}/mailtest?mail=${mail.value}`)
+        const occupiedList = getJSON(`${url}/static/json/occupiedSeats.json`)
+        let alreadyReserved = false
+        for (const [k, v] of Object.entries(occupiedList)) {
+            if (mail.value == v.mail) {
+                alert("Vent nå litt!!!\nDu har allerede reservert et sete!")
+                alreadyReserved = true
+                break
+            }
+        }
+        if (!alreadyReserved && mailExists && seatSelected.state) {
+            response = getJSON(`${url}/reserveseat?name=${Name.value}&mail=${mail.value}&seat=${seatSelected.seat}`)
+            if (response.response == "Success") {
+                alert(`Sete reservert!\nEn Mail har blitt sendt til ${mail.value}.\nÅpne mailen for å bekrefte plassen din innen 5 minutter!`)
+            }
+            else if (response.response == "Already reserved") {
+                alert("Setet er allerede reservert!")
+                populateSeats()
+                seatSelected = {
+                    state: false,
+                    seat: ""
+                }
+            }
+            else if (response.response == "Invalid mail") {
+                alert("Oida! Denne mailen kunne ikke brukes")
+            }
+            else {
+                alert("Oisan!\nEn uventet feil har oppstått.\nVennligst prøv igjen.\nHvis problemet vedvarer, ta kontakt med Emil Christiansen.")
+                populateSeats()
+                seatSelected = {
+                    state: false,
+                    seat: ""
+                }
+            }
+        }
+        else if (!seatSelected.state) {
+            alert("Du må velge et sete!")
+        }
+        else if (!alreadyReserved) {
+            alert("Oida! Denne mailen kunne ikke brukes")
+        }
+    })
+    
+}seatSelectionhandler()
