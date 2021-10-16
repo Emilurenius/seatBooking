b937fe4a550c7c78091a24c6892242b7aaf2ce7f
Emil Christiansen <emilsen68@gmail.com>
2021-10-16 17:12:19 +0200

    Update reservedList.js

--- server/public/javascript/reservedList.js
+++ server/public/javascript/reservedList.js
@@ -1,10 +1,10 @@
 function reservedList() {
     const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Æ", "Ø", "Å"]
     const list = getJSON(`${url}/static/json/occupiedSeats.json`)
-    container = document.getElementById("container")
+    const container = document.getElementById("container")
     for (const [k, v] of Object.entries(list)) {
         const text = document.createElement("p")
         text.innerHTML = `Sete ${alphabet[k.split(':')[0]]}:${k.split(':')[1]} ==> ${v.name}`
-        list.appendChild(list)
+        container.appendChild(list)
     }
 }reservedList()
